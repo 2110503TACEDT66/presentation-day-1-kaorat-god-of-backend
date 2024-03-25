3771d49f8d342cac61898db41c20f53ff9bb8065
Auiz <nopphawit47374@gmail.com>
2024-03-25 08:29:26 +0700

    modify code

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api/v1/appointments', require('./routes/appointments'));
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on ${process.env.HOST}:${PORT}`));
 
 process.on('unhandledRejection', (err, promise) => {
     console.log(`Error: ${err.message}`);
